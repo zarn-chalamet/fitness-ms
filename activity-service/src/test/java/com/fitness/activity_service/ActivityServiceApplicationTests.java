641d78428dc9beb964d9b6f06a48d572f2fd1b22
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-08-08 00:56:57 +0700

    init activity service

---
+++ activity-service/src/test/java/com/fitness/activity_service/ActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.activity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
